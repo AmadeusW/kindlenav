7ef97a8e77b38118c14e2f35f552cbcbd51e91f7
Amadeusz Wieczorek <git@amadeusw.com>
2016-04-20 00:21:52 -0700

    adds server implementation

---
+++ src/AmadeusW.KindleNav/Server/WebServer.cs
@@ -0,0 +1,27 @@
+﻿using Devkoes.Restup.WebServer.File;
+using Devkoes.Restup.WebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmadeusW.KindleNav.Server
+{
+    public static class WebServer
+    {
+        static HttpServer _httpServer;
+
+        public static async Task<bool> Start()
+        {
+            var httpServer = new HttpServer(1337);
+            _httpServer = httpServer;
+            
+            httpServer.RegisterRoute(new StaticFileRouteHandler(@"Server\www"));
+            await httpServer.StartServerAsync();
+
+            // Dont release deferral, otherwise app will stop
+            return true;
+        }
+    }
+}
